ce90762e9a1ad347e3d3b0c755b0269649598bdb
Stanislav Tarasov <iluvstrwrs@mail.ru>
2022-02-21 17:13:31 +0300

    Initial commit 2022_02_21_1713

--- src/main/java/com/javamaster/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/javamaster/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
-    ////
+    //New
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityJwtApplication.class, args);
     }
